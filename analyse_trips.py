c9b687a3f6a8032c6954402cc9f5cf78e0cf6653
Louis-Justin TALLOT <lj.tallot@gmail.com>
2024-04-28 18:58:44 +0200

    WIP : get back clean label + colors

--- analyse_trips.py
+++ analyse_trips.py
@@ -16,27 +16,33 @@ def plot_interactive_map(gdf: gpd.GeoDataFrame, show: bool = False):
 def unpack_tag(tag_series: str) -> pd.DataFrame:
     list_tags = [json.loads(tag) for tag in tag_series]
 
-    tag_names = []
-    for tag in list_tags:
-        tag_names.append([x["name"] for x in tag])
-
+    names = []
     colors = []
+    tag_color_mapping = {}
     for tag in list_tags:
-        colors.append([x["color"] for x in tag])
+        tag_names = []
+        tag_colors= []
+        for x in tag:
+            tag_names.append(x["name"])
+            tag_colors.append(x["color"])
+            tag_color_mapping[x["name"]] = x["color"]
+        names.append(tag_names)
+        colors.append(tag_colors)
 
-    return pd.DataFrame({"type": pd.Series(tag_names), "color": pd.Series(colors)})
+    return pd.DataFrame({"type": pd.Series(names), "color": pd.Series(colors)}), tag_color_mapping
 
 
 def clean_mapstr_data(gdf: gpd.GeoDataFrame) -> gpd.GeoDataFrame:
     gdf = gdf.copy()
     gdf.drop(columns="userComment", inplace=True)
     gdf.drop(columns="icon", inplace=True)
-    gdf.join(unpack_tag(gdf["tags"]))
-    
-    
-    
-def mapstr_stats(gpd: gpd.GeoDataFrame)-> str:
+    gdf_tags, tag_color_mapping = unpack_tag(gdf["tags"])
+    gdf.join(gdf_tags)
+    print(tag_color_mapping)
+
+
+def mapstr_stats(gpd: gpd.GeoDataFrame) -> str:
     stats = ""
     stats += f"Number of places visited: {len(gpd)}\n"
-    
+
     return stats

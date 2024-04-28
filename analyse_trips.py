76501878abeea1096ea8ca45ed3a2e802f2c4788
Louis-Justin TALLOT <lj.tallot@gmail.com>
2024-04-28 21:33:29 +0200

    WIP Number of départements and countries visited

--- analyse_trips.py
+++ analyse_trips.py
@@ -4,6 +4,9 @@ import shapely as shp
 import matplotlib.pyplot as plt
 import json
 
+FRENCH_DEPARTEMENTS = "data/french-departements.geojson"
+WORLD_COUNTRIES = "data/world-administrative-boundaries.geojson"
+
 
 def load_data(data_location: str) -> gpd.GeoDataFrame:
     return gpd.read_file(data_location)
@@ -21,7 +24,7 @@ def unpack_tag(tag_series: str) -> pd.DataFrame:
     tag_color_mapping = {}
     for tag in list_tags:
         tag_names = []
-        tag_colors= []
+        tag_colors = []
         for x in tag:
             tag_names.append(x["name"])
             tag_colors.append(x["color"])
@@ -29,7 +32,28 @@ def unpack_tag(tag_series: str) -> pd.DataFrame:
         names.append(tag_names)
         colors.append(tag_colors)
 
-    return pd.DataFrame({"type": pd.Series(names), "color": pd.Series(colors)}), tag_color_mapping
+    return (
+        pd.DataFrame({"type": pd.Series(names), "color": pd.Series(colors)}),
+        tag_color_mapping,
+    )
+
+
+def add_departement_info(
+    gdf: gpd.GeoDataFrame, departements_gdf: gpd.GeoDataFrame
+) -> gpd.GeoDataFrame:
+    joined = gpd.sjoin(gdf, departements_gdf, how="inner", predicate="intersects")
+    return gdf.copy()
+
+
+def add_world_info(
+    gdf: gpd.GeoDataFrame, world_countries_gdf: gpd.GeoDataFrame
+) -> gpd.GeoDataFrame:
+    gdf = gdf.copy()
+    joined = gpd.sjoin(gdf, world_countries_gdf, how="inner", predicate="intersects")
+    gdf["country"] = joined["name_right"]
+    gdf["country_FR"] = joined["french_short"]
+    gdf["continent"] = joined["continent"]
+    return gdf.copy()
 
 
 def clean_mapstr_data(gdf: gpd.GeoDataFrame) -> gpd.GeoDataFrame:
@@ -40,9 +64,16 @@ def clean_mapstr_data(gdf: gpd.GeoDataFrame) -> gpd.GeoDataFrame:
     gdf.join(gdf_tags)
     print(tag_color_mapping)
 
+    gdf = add_world_info(gdf)
+    gdf = add_departement_info(gdf)
+
 
 def mapstr_stats(gpd: gpd.GeoDataFrame) -> str:
     stats = ""
     stats += f"Number of places visited: {len(gpd)}\n"
+    stats += f"Numer of countries visited: \n"
+    stats += f"Number of continents visited: \n"
+    stats += f"Number of French départements visited \n"
+    stats += f"Number of US states visited \n"
 
     return stats
